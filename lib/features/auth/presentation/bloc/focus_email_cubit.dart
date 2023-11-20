607c0fc3ac8b1069f3844b2a5594b53014747c24
Joseph Lugo <josephfronted1@gmail.com>
2023-11-20 18:42:29 -0500

    refactor: delete unused and move file

--- lib/features/auth/presentation/bloc/focus_email_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class FocusValidation extends Cubit<bool> {
-  FocusValidation() : super(false);
-
-  void focus() => emit(true);
-  void unfocus() => emit(false);
-}
--- lib/features/auth/presentation/bloc/splash_screen_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class SplashScreenCubit extends Cubit<bool> {
-  SplashScreenCubit() : super(false) {
-    Timer(const Duration(seconds: 2), () => emit(true));
-  }
-}
